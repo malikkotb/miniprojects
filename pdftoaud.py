f043288521818bec3c380971852da864c96bb142
Malik Kotb <ge82vat@mytum.de>
2022-12-20 09:56:00 +0100

    audiobook maker from pdf

---
+++ pdftoaud.py
@@ -0,0 +1,18 @@
+import PyPDF3, pyttsx3, pdfplumber
+
+file = open('web_scraping/test.pdf', 'rb')
+pdfReader = PyPDF3.PdfFileReader(file)
+num_pages = pdfReader.numPages
+clean_text = ""
+with pdfplumber.open(file) as pdf:
+    for i in range(0, num_pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        clean_text += text
+print(clean_text)
+
+
+converter = pyttsx3.init()
+#converter.say(clean_text)
+converter.save_to_file(clean_text, 'test.mp3')
+converter.runAndWait()
